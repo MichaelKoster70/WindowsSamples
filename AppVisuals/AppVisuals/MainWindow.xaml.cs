0f6669adf5d9aeea3b09804ef20286be13278391
Michael Koster <MichaelKoster70@users.noreply.github.com>
2024-03-22 22:29:09 +0100

    Added sample for MSIX installer and its visual

---
+++ AppVisuals/AppVisuals/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright company="Michael Koster">
+//   Copyright (c) Michael Koster. All rights reserved.
+//   Licensed under the MIT License.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using System.Windows;
+
+namespace AppVisuals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
